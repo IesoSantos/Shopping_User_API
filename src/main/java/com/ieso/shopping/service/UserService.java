2b0f14423428fb1eb4824b5dd2abb4ff5d9a7e17
IesoSantos <andersondrs@hotmail.com>
2021-12-01 09:05:26 -0300

    5 Commit Insercao dos metodos delete, findByCPF e queryByName

--- src/main/java/com/ieso/shopping/service/UserService.java
+++ src/main/java/com/ieso/shopping/service/UserService.java
@@ -5,6 +5,7 @@ package com.ieso.shopping.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,35 @@ public class UserService {
 			return UserDTO.convert(usuario.get());
 		}
 		return null;
+	}
+	
+	public UserDTO save(UserDTO userDTO) {
+		User user = userRepository.save(User.convert(userDTO));
+		return UserDTO.convert(user);
+	}
+	
+	public UserDTO delete(long userId) {
+		Optional<User> user = userRepository.findById(userId);
+		if(user.isPresent()) {
+			userRepository.delete(user.get());
+		}
+		return null;
+	}
+	
+	public UserDTO findByCpf(String cpf) {
+		User user = userRepository.findByCpf(cpf);
+		if(user != null) {
+			return UserDTO.convert(user);
+		}
+		return null;
+	}
+	
+	public List<UserDTO> queryByName(String name){
+		List<User> usuarios = userRepository.queryByNomeLike(name);
+		return usuarios
+				.stream()
+				.map(UserDTO::convert)
+				.collect(Collectors.toList());
 	}
 
 }
