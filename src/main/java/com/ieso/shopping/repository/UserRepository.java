c583713a938443c34b0b36c9730327f032e19fb4
IesoSantos <andersondrs@hotmail.com>
2021-11-30 23:23:16 -0300

    3 Commit Adicao de UserService

--- src/main/java/com/ieso/shopping/repository/UserRepository.java
+++ src/main/java/com/ieso/shopping/repository/UserRepository.java
@@ -6,6 +6,7 @@ package com.ieso.shopping.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ieso.shopping.model.User;
 
@@ -13,6 +14,7 @@ import com.ieso.shopping.model.User;
  * @author Anderson dos Reis Santos
  *
  */
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 	User findByCpf(String cpf);
---
+++ src/main/java/com/ieso/shopping/service/UserService.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.ieso.shopping.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ieso.shopping.model.UserDTO;
+import com.ieso.shopping.model.User;
+import com.ieso.shopping.repository.UserRepository;
+
+/**
+ * @author Anderson dos Reis Santos
+ *
+ */
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository  userRepository;
+	
+	public List<UserDTO> getAll(){
+		List<User> usuarios = userRepository.findAll();
+		return usuarios
+				.stream()
+				.map(UserDTO::convert)
+				.collect(Collectors.toList());
+				
+	}
+
+}
