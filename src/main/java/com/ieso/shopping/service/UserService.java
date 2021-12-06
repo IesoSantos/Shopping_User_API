14d7c7520ece332cbbf988f1bb8d67dc1a9cfe3b
IesoSantos <andersondrs@hotmail.com>
2021-12-06 19:40:14 -0300

    Commit para atualização de pequenas alterações

--- src/main/java/com/ieso/shopping/service/UserService.java
+++ src/main/java/com/ieso/shopping/service/UserService.java
@@ -5,7 +5,6 @@ package com.ieso.shopping.service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
